b39316fd23aa67e739b23c15b17c3a0a482c2c09
Cameron Horwood <cameron.horwood@VisionCloudOne.local>
2025-09-18 15:07:02 -0300

    Add local and prod env. variables

--- src/AuthConfig.ts
+++ src/AuthConfig.ts
@@ -5,7 +5,7 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: "37fe86e2-3663-46bf-85ac-9efa15eeaa4d",
     authority: "https://login.microsoftonline.com/a0e0d277-5bc2-4964-bbc5-af91a166fe98",
-    redirectUri: "https://lively-dune-044aff80f.1.azurestaticapps.net",
+    redirectUri: `${import.meta.env.VITE_REDIRECT_URI}`,
   },
   cache: {
     cacheLocation: "localStorage",
